b38eec148ea826b7e4de9fc51e73021ebde80537
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-09-24 10:33:55 +0300

    feat:  random pic gallery

---
+++ museum/src/script/gallery.js
@@ -0,0 +1,32 @@
+const pictureInnerContainer = document.querySelector('.collection-inner');
+let arrPic = []
+function getImg() {
+    console.log(pictureInnerContainer.querySelectorAll('img'));
+
+    pictureInnerContainer.querySelectorAll('img')
+        .forEach((pic, i, a) => {
+            let arrEl = []
+            arrEl.push(pic.src)
+            arrEl.push(pic.className)
+            arrPic.push(arrEl)
+        })
+}
+const addImg = function () {
+    pictureInnerContainer.innerHTML = `<span class="empty"></span>
+    <span class="empty2"></span>`
+    for (let i = 0; i < arrPic.length; i++) {
+        const img = document.createElement('img');
+        img.classList.add(arrPic[i][1])
+        img.src = `${arrPic[i][0]}`;
+        img.alt = `galery${i}`;
+        pictureInnerContainer.append(img);
+    }
+
+}
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+getImg()
+shuffle(arrPic)
+addImg()
