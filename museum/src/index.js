d3633c20f117784ba65adce00a0dee53e8b02fcc
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-09-24 16:24:07 +0300

    feat: video slader

--- museum/src/index.js
+++ museum/src/index.js
@@ -1,21 +1,16 @@
 import "./scss/main.scss"
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
-
+import './script/gallery'
 // Инициализация свайпера
 const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
     },
-
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    /* effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    }, */
     loop: true,
     speed: 1000,
     spaceBetween: 150,
@@ -31,3 +26,17 @@ swiper.on('slideChange', function () {
     let currentSlide = ++swiper.realIndex
     currentSlides.innerHTML = '0' + currentSlide
 })
+
+
+// Инициализация видео свайпера
+const swiperVideo = new Swiper('.swiper-video', {
+    pagination: {
+        el: '.swiper-video-pagination',
+    },
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 42,
+    mousewheel: {
+        sentivity: 1,
+    },
+});
