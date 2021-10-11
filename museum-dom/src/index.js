3061517e0d4754337ef7e5b68da55d32576e5383
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-10-11 16:23:58 +0300

    feat: mapbox

--- museum-dom/src/index.js
+++ museum-dom/src/index.js
@@ -4,8 +4,12 @@ import 'swiper/css/bundle';
 import './script/gallery'
 import './script/video-player'
 import initComparisons from './script/picSlide'
+import './script/mapbox'
+import { removeStyle } from './script/video-player'
+import './script/ticketCalc'
+import './script/up_button'
+// setTimeout(() => , 110)
 
-setTimeout(() => initComparisons(), 110)
 
 // Инициализация свайпера
 const swiper = new Swiper('.swiper', {
@@ -21,9 +25,7 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     speed: 1000,
     spaceBetween: 150,
-    mousewheel: {
-        sentivity: 1,
-    },
+
 });
 
 // Кастомная фракция
@@ -68,84 +70,26 @@ const swiperVideo = new Swiper('.swiper-video', {
         },
     },
 
-    mousewheel: {
-        sentivity: 1,
-    },
+
 });
 
 ///
-// function initComparisons() {
-//     var x, i;
-//     x = document.querySelector(".img-comp-overlay");
-//     compareImages(x);
-
-//     function compareImages(img) {
-//         var slider, img, clicked = 0, w, h;
-//         w = img.offsetWidth;
-//         h = img.offsetHeight;
-//         img.style.width = (w / 2) + "px";
-//         slider = document.createElement("DIV");
-//         slider.setAttribute("class", "img-comp-slider");
-//         img.parentElement.insertBefore(slider, img);
-//         slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
-//         slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
-//         slider.addEventListener("mousedown", slideReady);
-//         window.addEventListener("mouseup", slideFinish);
-//         slider.addEventListener("touchstart", slideReady);
-//         window.addEventListener("touchend", slideFinish);
-//         function slideReady(e) {
-//             e.preventDefault();
-//             clicked = 1;
-//             window.addEventListener("mousemove", slideMove);
-//             window.addEventListener("touchmove", slideMove);
-//         }
-//         function slideFinish() {
-//             clicked = 0;
-//         }
-//         function slideMove(e) {
-//             var pos;
-//             if (clicked == 0) return false;
-//             pos = getCursorPos(e)
-//             if (pos < 0) pos = 0;
-//             if (pos > w) pos = w;
-//             slide(pos);
-//         }
-//         function getCursorPos(e) {
-//             var a, x = 0;
-//             e = (e.changedTouches) ? e.changedTouches[0] : e;
-//             a = img.getBoundingClientRect();
-//             x = e.pageX - a.left;
-//             x = x - window.pageXOffset;
-//             return x;
-//         }
-//         function slide(x) {
-//             img.style.width = x + "px";
-//             slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
-//         }
-//     }
-// }
-
-console.log(`
-самооценка - 152 балла (?8) 
-:
-Не проверил результат проверки скорости сайта для мобильных устройств на момент деплоя.
-Остальное запилил.
-Хочу напомнить, что проверять разрешение нужно в mobile режиме, т.к. разрешения до 1024 для скрин-экранов(а на декстопе присуствует полоса прокрутки(16-18 пикселей в зависимости от браузера), что может решать для оценки по ПиксельПефекту)
-Так же может сразу не перестроится, поэтому можно обнавить или переключить пару раз между мобильным и декстопным модом.
-
-Удачи в следующих заданиях!
+const video = document.querySelector('.viewer')
+const swiperV = document.querySelector('.swiper-video')
 
 
-`);
+swiperVideo.on('slideChangeTransitionEnd', function () {
+    video.src = `${swiperV.querySelector('.swiper-slide-active').dataset.url}`
+    video.poster = `${swiperV.querySelector('.swiper-slide-active').dataset.poster}`
+    const progress = document.querySelector('.progress')
+    removeStyle()
 
+});
 
 
 /////
 const menu = e => {
-    // if (!e.target.closest('.adaptive') || !e.target.closest('.menu_link')) return
-    // document.querySelector('.nav-menu').classList.toggle('nav-menu_active')
     document.querySelector('.menu_popup').classList.toggle('menu_popup_active')
-    // document.querySelector('.menu_popup_content').classList.toggle('menu_popup_content_active')
     document.querySelector('.adaptive').classList.toggle('adaptive_close')
     document.querySelector('.welcome-content').classList.toggle('welcome-content_close')
 }
@@ -172,8 +116,12 @@ document.querySelector('.popup__area').addEventListener('click', (e) => {
 })
 document.addEventListener('click', e => {
     if (e.target.closest('.menu_popup') || e.target.closest('.adaptive_close')) return
+    if (!document.querySelector('.menu_popup_active')) return
     document.querySelector('.menu_popup_active').classList.remove('menu_popup_active')
     document.querySelector('.adaptive_close').classList.remove('adaptive_close')
     document.querySelector('.welcome-content_close').classList.remove('welcome-content_close')
-    console.log(1);
+})
+
+window.addEventListener('load', () => {
+    initComparisons()
 })
--- museum-dom/src/script/mapbox.js
+++ museum-dom/src/script/mapbox.js
@@ -1,10 +1,30 @@
+import mapboxgl from 'mapbox-gl';
 
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+let markers = [[2.3333, 48.8602],
+[2.3397, 48.8607],
+[2.3330, 48.8619],
+[2.3365, 48.8625]]
 
 mapboxgl.accessToken = 'pk.eyJ1IjoieXVyaWNocHJvbG9sIiwiYSI6ImNrdWg2eDRqYTA5czMybm96NzE5bmZlMnQifQ.LvI5gAVW-_FMT63iCwRBTw';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/yurichprolol/ckuh7h6361nhe17s0oc75r80g', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    style: 'mapbox://styles/yurichprolol/ckuh7h6361nhe17s0oc75r80g',
+    center: [2.3364, 48.86091], // starting position [lng, lat]
+    zoom: 16, // starting zoom
 });
+
+const nav = new mapboxgl.NavigationControl({
+    showZoom: true
+})
+
+// Create a new marker.
+const marker = new mapboxgl.Marker({ color: '#000000' })
+    .setLngLat([2.3364, 48.86091])
+    .addTo(map);
+
+
+markers.forEach((marker) => {
+    new mapboxgl.Marker({ color: '#b5b0a9' })
+        .setLngLat(marker)
+        .addTo(map);
+})
