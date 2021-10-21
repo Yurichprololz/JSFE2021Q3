1f9bdbd86343ea5b9c0dc2c2f337256a5b3fae3d
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-10-21 22:08:24 +0300

    feat: progressive player

--- momentum/src/script/audioPlayer.js
+++ momentum/src/script/audioPlayer.js
@@ -4,6 +4,9 @@ const PLAY_BTN = document.querySelector('.audio__play')
 const PREV_BTN = document.querySelector('.audio__prev')
 const NEXT_BTN = document.querySelector('.audio__next')
 const PLAY_LIST = document.querySelector('.audio__list')
+const progress = document.querySelector('.progress')
+const progressVolume = document.querySelector('.progress-vol')
+const volBtn = document.querySelector('.audio__vol')
 let isPlay = false
 let count = 0
 
@@ -92,11 +95,49 @@ function createList() {
         PLAY_LIST.append(conteiner)
     })
 }
-createList()
 
+function changeTime() {
+    AUDIO.currentTime = (progress.value * AUDIO.duration) / 100
+}
+function timeUp() {
+    progress.value = (AUDIO.currentTime / AUDIO.duration) * 100
+    if (!AUDIO.currentTime) {
+        progress.value = '0'
+    }
+    if (progress.value == 100) {
+        // toggleStyle()
+        progress.value = 0
+    }
+}
+function changeVol() {
+    AUDIO.volume = progressVolume.value
+    if (AUDIO.volume == 0) {
+        volBtn.classList.add('audio__vol_mute')
+    } else {
+        if (volBtn.classList.contains('audio__vol_mute')) {
+            volBtn.classList.remove('audio__vol_mute')
+
+        }
+    }
+}
+function toggleVolume() {
+    if (volBtn.classList.contains('audio__vol_mute')) {
+        progressVolume.value = 0.5
+        changeVol()
+    } else {
+        progressVolume.value = 0
+        changeVol()
+    }
+}
+
+createList()
+volBtn.addEventListener('click', toggleVolume)
 PLAY_BTN.addEventListener('click', playA)
 PREV_BTN.addEventListener('click', prevSound)
 NEXT_BTN.addEventListener('click', nextSound)
+progress.addEventListener('change', changeTime)
+progressVolume.addEventListener('change', changeVol)
+AUDIO.addEventListener('timeupdate', timeUp)
 setInterval(() => {
     if (AUDIO.currentTime === AUDIO.duration) {
         nextSound()
