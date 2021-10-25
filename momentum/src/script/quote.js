4cd4c234751b7610daa39ce4ed7d7bff329ed052
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-10-25 17:50:08 +0300

    feat: translate quote on RU

--- momentum/src/script/quote.js
+++ momentum/src/script/quote.js
@@ -1,9 +1,11 @@
 const BTN = document.querySelector('.quote__btn')
 const TEXT = document.querySelector('.quote__text')
 const AUTHOR = document.querySelector('.quote__author')
+const LANG = document.querySelectorAll("input[name='lang']")
+
+
 async function getQuotes() {
-    debugger
-    const quotes = './script/quote.json';
+    const quotes = LANG[0].checked === true ? './script/quote.json' : './script/quote_RU.json';
     const res = await fetch(quotes);
     const data = await res.json();
     const i = getRandom(data)
@@ -16,3 +18,24 @@ function getRandom(data) {
     return Math.floor(Math.random() * (data.length))
 }
 BTN.addEventListener('click', getQuotes)
+LANG.forEach(el => el.addEventListener('change', getQuotes))
+
+function setLocalStorage() {
+    if (LANG[0].checked === true) {
+        localStorage.setItem('lang', 'en');
+    } else {
+        localStorage.setItem('lang', 'ru');
+
+    }
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    if (localStorage.getItem('lang') === 'en') {
+        LANG[0].checked = true;
+    } else {
+        LANG[1].checked = true
+        getQuotes()
+    }
+}
+window.addEventListener('load', getLocalStorage)
