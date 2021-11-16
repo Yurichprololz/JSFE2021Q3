33ef5bf45f2f3efc6df4a031946fd1c61c579669
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-11-16 14:53:17 +0300

    feat: render menu

---
+++ art-quiz/src/assets/scripts/_menu.js
@@ -0,0 +1,26 @@
+import { AuthorGame, nextRound } from './_author_game'
+
+const WRAP = document.querySelector('.main')
+const renderMenu = () => {
+    WRAP.innerHTML = `<div class="wrap menu">
+    <div class="menu__start">
+      <h3 class="menu__title">Mode quiz</h3>
+      <div class="menu__modes">
+        <div class="menu__mode menu__button">
+          <p>Artists quiz</p>
+          <div class="menu__buttonIco menu__buttonIco_artists"></div>
+        </div>
+
+        <div class="menu__mode menu__button">
+          <p>Pictures quiz</p>
+          <div class="menu__buttonIco menu__buttonIco_picture"></div>
+        </div>
+      </div>
+    </div>
+    <div class="menu__setting menu__button">
+      <p>Setting</p>
+    </div>`
+    document.querySelector('.menu__buttonIco_artists').addEventListener('click', nextRound)
+
+}
+export { renderMenu }
