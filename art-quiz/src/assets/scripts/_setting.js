567f7d553b35e493dd61d5323c34af705aa36c0e
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-11-18 23:24:50 +0300

    feat: chaning setting

--- art-quiz/src/assets/scripts/_setting.js
+++ art-quiz/src/assets/scripts/_setting.js
@@ -13,22 +13,52 @@ const showSetting = () => {
 const hideSetting = () => {
     POPUP.classList.remove('popup__setting_active')
 }
-
-RANGES.forEach(input => {
-    const OUTPUT = document.querySelector(`#${input.id}_output`)
-    OUTPUT.textContent = input.value + ' sec'
-    input.addEventListener('input', showDigitalTime)
+CHECKBOXS.forEach(el => {
+    el.addEventListener('change', (e) => {
+        const range = document.getElementById(`${e.target.id.split('-')[0]}`)
+        if (e.target.checked === false) {
+            range.value = 0
+        } else {
+            range.value = localStorage.getItem(`${range.id}`)
+        }
+        range.dispatchEvent(update)
+    })
 })
+RANGES.forEach(input => {
+    input.addEventListener('input', showDigital)
+    input.addEventListener('change', (e) => {
+        const range = e.target
+        localStorage.setItem(`${range.id}`, range.value);
 
-function showDigitalTime(event) {
-    const el = event.target
-    const OUTPUT = document.querySelector(`#${el.id}_output`)
-    OUTPUT.textContent = el.value + ' sec'
+    })
+})
+const cheinRangeWithCheck = INPUT => {
+    const check = document.getElementById(`${INPUT.id}-check`)
+    if (INPUT.value > 0) {
+        check.checked = true
+    } else {
+        check.checked = false
+    }
+
 }
+
+function showDigital(event) {
+    const INPUT = event.target
+    const OUTPUT = document.getElementById(`${INPUT.id}_output`)
+
+    if (INPUT.id == 'time_for_round') {
+        OUTPUT.innerHTML = INPUT.value < 10 ? '0' + INPUT.value + ' sec' : INPUT.value + ' sec'
+    } else {
+        OUTPUT.innerHTML = INPUT.value < 10 ? '0' + INPUT.value + ' per' : INPUT.value + ' per'
+    }
+
+    cheinRangeWithCheck(INPUT)
+}
+
 function setLocalStorage() {
 
     RANGES.forEach((range, i) => {
-        localStorage.setItem('range' + i, range.value);
+        localStorage.setItem(`${range.id}`, range.value);
     })
     CHECKBOXS.forEach(checkbox => {
 
@@ -36,17 +66,22 @@ function setLocalStorage() {
 }
 
 function getAudio(i) {
+    const volume = document.getElementById('system_sounds').value
     let audio = AUDIOS[i]
+    audio.volume = volume / 100
     audio.currentTime = 0
     audio.play()
 }
 function getLocalStorage() {
 
     RANGES.forEach((range, i) => {
-        if (localStorage.getItem('range' + i)) {
-            range.value = localStorage.getItem('range' + i);
+        if (localStorage.getItem(`${range.id}`)) {
+            range.value = localStorage.getItem(`${range.id}`);
             range.dispatchEvent(update)
         }
     })
 }
+
+// document.getElementById('time_for_round').addEventListener('input', showDigital)
+
 export { showSetting, hideSetting, setLocalStorage, getLocalStorage, getAudio }
