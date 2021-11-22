5f760854a74496dd37e44c0a2fc902691960555e
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-11-22 20:22:06 +0300

    fix: path JSON

--- art-quiz/src/assets/scripts/_request_JSON.js
+++ art-quiz/src/assets/scripts/_request_JSON.js
@@ -1,5 +1,5 @@
 async function getInfo() {
-    const info = '../../images.json';
+    const info = './images.json';
     const res = await fetch(info);
     const data = await res.json();
     return data
