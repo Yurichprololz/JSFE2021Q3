c21dc4421251943756ba93cdb7d13f37c019b2df
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-11-28 19:49:18 +0300

    init: new JS

---
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: '0962f003e4114ee19bbd1f20d04ccddf',
+        })
+    }
+}
+
+export default AppLoader;
