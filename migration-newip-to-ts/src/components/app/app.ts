74c9e0eb200e1f63e8cb9af279b90d7a59e4dfbc
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-12-06 18:45:54 +0300

    refactor: add type

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -1,6 +1,6 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
-import{Inews, Isources} from '../../interfaces'
+import{Inews, Isources, Irequest} from '../../interfaces'
 
 interface IApp{
     start():void 
@@ -17,11 +17,11 @@ class App implements IApp{
   start():void {
     document
       ?.querySelector('.sources')
-      ?.addEventListener('click', (e) => this.controller.getNews(e, (data:Inews | Isources):void =>{
+      ?.addEventListener('click', (e) => this.controller.getNews(e, (data:Irequest):void =>{
         if('totalResults' in data) this.view.drawNews(data)
       }
       ));
-    this.controller.getSources((data:Inews |Isources):void =>{
+    this.controller.getSources((data:Irequest):void =>{
       if('sources' in data)this.view.drawSources(data)
     } );
   }
