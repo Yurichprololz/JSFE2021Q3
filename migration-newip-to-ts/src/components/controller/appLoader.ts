c4d3665ec0cd7b304dfdfb09f5b9ee120170b5c9
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-12-07 00:35:55 +0300

    feat: add link for deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '0962f003e4114ee19bbd1f20d04ccddf',
         });
     }
