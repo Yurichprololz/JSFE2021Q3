c925bf4ce66a45bf95eec1a878b9a1b38057d34c
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-12-22 21:31:49 +0300

    feat: get changing tree

---
+++ chrismas-task-part2/src/scripts/change-tree.ts
@@ -0,0 +1,11 @@
+const changeTreeHandle = (e: Event): void => {
+  const tree = document.getElementById("tree") as HTMLImageElement;
+  let target = e.target as HTMLDivElement;
+  target = target.closest("div") as HTMLDivElement;
+  tree.src = `./assets/images/tree/${target.dataset.path}.png`;
+};
+
+const button = document.querySelectorAll(".tree-wrap_tree") as unknown as HTMLDivElement[];
+button.forEach((element) => {
+  element.addEventListener("click", changeTreeHandle);
+});
