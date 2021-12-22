30a7824b0ed1a815caaa6c1210cd554314a9c4df
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2021-12-22 21:31:31 +0300

    feat: get changing fon

---
+++ chrismas-task-part2/src/scripts/change-fon.ts
@@ -0,0 +1,15 @@
+const changeFonHandle = (e: Event): void => {
+  const fon = document.querySelector(".tree") as HTMLDivElement;
+  let target = e.target as HTMLDivElement;
+  target = target.closest("div") as HTMLDivElement;
+  const img = new Image();
+  img.src = `../assets/images/bg/${target.dataset.path}.jpg`;
+  img.onload = () => {
+    fon.style.backgroundImage = `url(${img.src})`;
+  };
+};
+
+const button1 = document.querySelectorAll(".fon-wrap__item") as unknown as HTMLDivElement[];
+button1.forEach((element) => {
+  element.addEventListener("click", changeFonHandle);
+});
