7b9d33b755a297d965eff069432ef35aef4b4ee9
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-01-24 21:45:31 +0300

    feat: self-assessment

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -4,3 +4,8 @@ import { renderGarage } from './scripts/render-garage';
 
 renderBasicLayout();
 renderGarage();
+
+console.log(`
+Требования кросс чека выполнены полностью, 
+возможно Вы сможете найти ошибки, но и по всем вопросам Вы можете написать 
+мне в Дискорд Юрий(@Yurichprololz)`);
